e2e34ad253ed1ba94e3479e9888f21ba4991c8e1
John-thabuks <thabuksjohn@gmail.com>
2024-03-18 22:37:17 +0300

    correction

--- server/app.py
+++ server/app.py
@@ -1,41 +1,90 @@
+# #!/usr/bin/env python3
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return f"<h1>Python Operations with Flask Routing and Views</h1>"
+
+# @app.route("/print/<parameter>")
+# def print_string(parameter):
+#     print(parameter)
+#     return parameter
+
+# @app.route('/count/<int:num>')
+# def count(num):
+#     res = ""
+#     for i in range(num):
+#         res+= str(i) + "\n"
+#     return res
+
+# @app.route('/math/<int:num1>/<operation>/<int:num2>')
+# def math(num1, operation, num2):
+#     if operation=='+':
+#         result=num1+num2
+#     elif operation=='-':
+#         result=num1-num2
+#     elif operation=='*':
+#         result=num1*num2
+#     elif operation=='div':
+#         result=num1/num2
+#     elif operation=='%':
+#         result=num1%num2
+#     else:
+#         result="invalid operation"
+#     return str(result)
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
 #!/usr/bin/env python3
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+# Your index() view should be routed to at the base URL with /. It should Contain an h1 element that contains the title of this application, "Python Operations with Flask Routing and Views".
+@app.route('/')
 def index():
-    return f"<h1>Python Operations with Flask Routing and Views</h1>"
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route("/print/<parameter>")
+# A print_string view should take one parameter, a string. It should print the string in the console and display it in the web browser. Its URL should be of the format /print/parameter.
+@app.route('/print/<string:parameter>')
 def print_string(parameter):
     print(parameter)
     return parameter
 
-@app.route('/count/<int:num>')
-def count(num):
-    res = ""
-    for i in range(num):
-        res+= str(i) + "\n"
-    return res
+# A count() view should take one parameter, an integer. It should display all numbers in the range of that parameter on separate lines. Its URL should be of the format /count/parameter.
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
 
-@app.route('/math/<int:num1>/<operation>/<int:num2>')
-def math(num1, operation, num2):
-    if operation=='+':
-        result=num1+num2
-    elif operation=='-':
-        result=num1-num2
-    elif operation=='*':
-        result=num1*num2
-    elif operation=='div':
-        result=num1/num2
-    elif operation=='%':
-        result=num1%num2
-    else:
-        result="invalid operation"
-    return str(result)
+# A math() view should take three parameters: num1, operation, and num2. It must perform the appropriate operation on the two numbers in the order that they are presented. The included operations should be: +, -, *, div (/ would change the URL path), and %. Its URL should be of the format /math/<num1><operation><num2>.
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    if operation == '+':
+        return str(num1 + num2)
+    
+    elif operation == '-':
+        return str(num1 - num2)
 
+    elif operation == '*':
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        return str(num1 / num2)
+
+    elif operation == '%':
+        return str(num1 % num2)
+
+    return 'Operation not recognized. Please use one of the following: + - * div %'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
